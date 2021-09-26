007c56df7c8e71d6d0a2d822362ebc5321c915e0
qhuy <quanghuy4919@gmail.com>
2021-09-26 12:40:19 +0700

    feat: layout content page

---
+++ Intern > HuyTDQ/static/js/content.js
@@ -0,0 +1,123 @@
+let mock_data = [
+    {
+        id: 0,
+        name: 'Onepiece',
+        preview_pic: '../static/media/list_post/post0',
+        author: 'QHuy',
+        created: '2021/09/24',
+        vols: '100',
+        title: 'Onepiece review',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: '../../static/media/background_post/post1',
+            content: `In this world, humans coexist with demonic predators called Yoma. These demonic beasts feast on human innards and can blend into human society by taking on human appearance. As a counter force, a mysterious organization created half-human, half-Yoma warriors known as the "Silver Eyed Witches" or "Claymores", after the huge claymore swords they carry. They are detested by humanity however necessary.
+
+            The story begins with a young boy, Raki, who has lost everything in a Yoma attack, and the Claymore, Clare, who is generally detested by society. Throughout the series, Clare and Raki show their deeper qualities, powers and Clare's never ending devotion to the goal she swore to fulfill in her childhood.
+            `,
+            profile_pic: './../static/media/profile_post/post1'
+
+        },
+        
+    },
+    {
+        id: 1,
+        name: '',
+        preview_pic: '../static/media/list_post/post1',
+        author: 'QHuy',
+        created: '',
+        vols: '100',
+        title: 'Claymore',
+        scores: '10.0',
+        link_direct: "./content/content1.html",
+        detail: {
+            background_pic: '../../static/media/background_post/post1',
+            content: `In this world, humans coexist with demonic predators called Yoma. These demonic beasts feast on human innards and can blend into human society by taking on human appearance. As a counter force, a mysterious organization created half-human, half-Yoma warriors known as the "Silver Eyed Witches" or "Claymores", after the huge claymore swords they carry. They are detested by humanity however necessary.
+
+            The story begins with a young boy, Raki, who has lost everything in a Yoma attack, and the Claymore, Clare, who is generally detested by society. Throughout the series, Clare and Raki show their deeper qualities, powers and Clare's never ending devotion to the goal she swore to fulfill in her childhood.
+            `,
+            profile_pic: './../static/media/profile_post/post1'
+
+        },
+    },
+    {
+        id: 2,
+        name: '',
+        preview_pic: '../static/media/list_post/post2', 
+        author: 'QHuy',
+        created: '2021/09/24',
+        vols: '100',
+        title: 'Assassination Classroom',
+        scores: '10.0',
+        link_direct: "./content/content1.html",
+        detail: {
+            background_pic: '../../static/media/background_post/post1',
+            content: `In this world, humans coexist with demonic predators called Yoma. These demonic beasts feast on human innards and can blend into human society by taking on human appearance. As a counter force, a mysterious organization created half-human, half-Yoma warriors known as the "Silver Eyed Witches" or "Claymores", after the huge claymore swords they carry. They are detested by humanity however necessary.
+
+            The story begins with a young boy, Raki, who has lost everything in a Yoma attack, and the Claymore, Clare, who is generally detested by society. Throughout the series, Clare and Raki show their deeper qualities, powers and Clare's never ending devotion to the goal she swore to fulfill in her childhood.
+            `,
+            profile_pic: './../static/media/profile_post/post1'
+
+        },
+    },
+    {
+        id: 3,
+        name: '',
+        preview_pic: '../static/media/list_post/post3',
+        author: 'QHuy',
+        created: '2021/09/24',
+        vols: '100',
+        title: 'Claymore',
+        scores: '10.0',
+        link_direct: "./content/content1.html",
+        detail: {
+            background_pic: '../../static/media/background_post/post1',
+            content: `In this world, humans coexist with demonic predators called Yoma. These demonic beasts feast on human innards and can blend into human society by taking on human appearance. As a counter force, a mysterious organization created half-human, half-Yoma warriors known as the "Silver Eyed Witches" or "Claymores", after the huge claymore swords they carry. They are detested by humanity however necessary.
+
+            The story begins with a young boy, Raki, who has lost everything in a Yoma attack, and the Claymore, Clare, who is generally detested by society. Throughout the series, Clare and Raki show their deeper qualities, powers and Clare's never ending devotion to the goal she swore to fulfill in her childhood.
+            `,
+            profile_pic: './../static/media/profile_post/post1'
+
+        },
+    },
+    {
+        id: 4,
+        name: '',
+        preview_pic: '../static/media/list_post/post4',
+        author: 'QHuy',
+        created: '2021/09/24',
+        vols: '100',
+        title: 'Claymore',
+        scores: '10.0',
+        link_direct: "./content/content1.html",
+        detail: {
+            background_pic: '../../static/media/background_post/post1',
+            content: `In this world, humans coexist with demonic predators called Yoma. These demonic beasts feast on human innards and can blend into human society by taking on human appearance. As a counter force, a mysterious organization created half-human, half-Yoma warriors known as the "Silver Eyed Witches" or "Claymores", after the huge claymore swords they carry. They are detested by humanity however necessary.
+
+            The story begins with a young boy, Raki, who has lost everything in a Yoma attack, and the Claymore, Clare, who is generally detested by society. Throughout the series, Clare and Raki show their deeper qualities, powers and Clare's never ending devotion to the goal she swore to fulfill in her childhood.
+            `,
+            profile_pic: './../static/media/profile_post/post1'
+
+        },
+    },
+    {
+        id: 5,
+        name: '',
+        preview_pic: '../static/media/list_post/post5',
+        author: 'QHuy',
+        created: '2021/09/24',
+        vols: '100',
+        title: 'Claymore',
+        scores: '10.0',
+        link_direct: "./content/content1.html",
+        detail: {
+            background_pic: '../../static/media/background_post/post1',
+            content: `In this world, humans coexist with demonic predators called Yoma. These demonic beasts feast on human innards and can blend into human society by taking on human appearance. As a counter force, a mysterious organization created half-human, half-Yoma warriors known as the "Silver Eyed Witches" or "Claymores", after the huge claymore swords they carry. They are detested by humanity however necessary.
+
+            The story begins with a young boy, Raki, who has lost everything in a Yoma attack, and the Claymore, Clare, who is generally detested by society. Throughout the series, Clare and Raki show their deeper qualities, powers and Clare's never ending devotion to the goal she swore to fulfill in her childhood.
+            `,
+            profile_pic: './../static/media/profile_post/post1'
+
+        },
+    }
+];
