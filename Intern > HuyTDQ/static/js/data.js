b7431233ecefde4ff0b26b630662f17e1ffe25f1
qhuy <quanghuy4919@gmail.com>
2021-09-30 23:59:02 +0700

    other: split mock_data to another js file

---
+++ Intern > HuyTDQ/static/js/data.js
@@ -0,0 +1,304 @@
+export var mock_data = [
+    {
+        id: 0,
+        name: 'Onepiece',
+        preview_pic: '../static/media/list_post/post0',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'Onepiece review',
+        scores: '8.5',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/banner/117802-Esqkc7DfRvfh.jpg',
+            content: `As a child, Monkey D. Luffy was inspired to become a pirate by listening to the tales of the buccaneer "Red-Haired" Shanks. But his life changed when Luffy accidentally ate the Gum-Gum Devil Fruit and gained the power to stretch like rubber...at the cost of never being able to swim again! Years later, still vowing to become the king of the pirates, Luffy sets out on his 
+            adventure...one guy alone in a rowboat, in search of the legendary "One Piece," said to be the greatest treasure in the world...
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx30013-oT7YguhEK1TE.jpg',
+            vols: '100',
+            start_date: 'Jul 22, 1997',
+            end_data: 'on going',
+            mangaka: 'Eiichirou Oda'
+
+
+        },
+        
+    },
+    {   
+        id: 1,
+        name: '',
+        preview_pic: "../static/media/list_post/post1",
+        author: 'QHuy',
+        created: '',
+        title: 'Claymore',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: '../../static/media/background_post/post1',
+            content: `In this world, humans coexist with demonic predators called Yoma. These demonic beasts feast on human innards and can blend into human society by taking on human appearance. As a counter force, a mysterious organization created half-human, half-Yoma warriors known as the "Silver Eyed Witches" or "Claymores", after the huge claymore swords they carry. They are detested by humanity however necessary.
+
+            The story begins with a young boy, Raki, who has lost everything in a Yoma attack, and the Claymore, Clare, who is generally detested by society. Throughout the series, Clare and Raki show their deeper qualities, powers and Clare's never ending devotion to the goal she swore to fulfill in her childhood.
+            `,
+            profile_pic: '../../static/media/profile_post/post1',
+            vols: '159',
+            start_date: 'May 6, 2001',
+            end_data: 'Oct 4, 2014',
+            mangaka: 'Norihiro Yagi'
+
+
+        },
+    },
+    {
+        id: 2,
+        name: '',
+        preview_pic: '../static/media/list_post/post2', 
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'Assassination Classroom',
+        scores: '8.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/anime/banner/20755-D4ipww9U8YkC.jpg',
+            content: `The students of class 3-E have a mission: kill their teacher before graduation. He has already destroyed the moon, and has promised to destroy the Earth if he can not be killed within a year. 
+            But how can this class of misfits kill a tentacled monster, capable of reaching Mach 20 speed, who may be the best teacher any of them have ever had?
+            `,
+            profile_pic: 'https://cdn.myanimelist.net/images/anime/12/64043l.jpg?_gl=1*1cpgxc9*_ga*MTA0MzI5MTA0LjE2MzIzMjYwNTI.*_ga_26FEP9527K*MTYzMjY2MTM5OS40LjEuMTYzMjY2MjU2My40Mg..',
+            vols: '21',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+
+
+        },
+    },
+    {
+        id: 3,
+        name: '',
+        preview_pic: '../static/media/list_post/post3',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'Haikyuu!!',
+        scores: '7.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/anime/banner/20464-HbmkPacki4sl.jpg',
+            content: `spired after watching a volleyball ace nicknamed "Little Giant" in action, small-statured Shouyou Hinata revives the volleyball club at his middle school. 
+            The newly-formed team even makes it to a tournament; however, their first match turns out to be their last when they are brutally squashed by the "King of the Court," Tobio Kageyama. Hinata vows to surpass Kageyama, and so after graduating from middle school, he joins Karasuno High School's volleyball team—only to find that his sworn rival, Kageyama, is now his teammate.
+
+            Thanks to his short height, Hinata struggles to find his role on the team, even with his superior jumping power. 
+            Surprisingly, Kageyama has his own problems that only Hinata can help with, and learning to work together appears to be the only way for the team to be successful. 
+            Based on Haruichi Furudate's popular shounen manga of the same name, Haikyuu!! is an exhilarating and emotional sports comedy following two determined athletes as they attempt to patch a heated rivalry in order to make their high school volleyball team the best in Japan.
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx20464-eW7ZDBOcn74a.png',
+            vols: '34',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+        },
+    },
+    {
+        id: 4,
+        name: '',
+        preview_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx86303-DItZrA3hbaJf.jpg',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'Kobayashi-san Chi no Maidragon S',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/anime/banner/107717-xi8BDn9kucOI.jpg',
+            content: `A strange turn of events leads the Dragon, Tohru, to work as Miss Kobayashi's maid. She occasionally (that's a lie, she often) causes trouble for her beloved Miss Kobayashi while blending into human society and splendidly (that's a lie, only mediocrely) carrying out her maid duties. Her fellow dragons, Kanna, Lucoa, Fafnir, and Elma all find their own places to fit in as well and enjoy interspecies interactions with the humans.
+            Yet while they're all enjoying that laid-back and occasionally turbulent left, the threat of a new Dragon swoops down upon Miss Kobayashi.
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx107717-bixaW1NTGBra.jpg',
+            vols: '12',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+
+        },
+    },
+    {
+        id: 5,
+        name: '',
+        preview_pic: '../static/media/list_post/post5',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'Dragonball',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/banner/30042-4aSSSOxCNWgE.jpg',
+            content: `A remastered version of Dragon Ball Z that adheres more to the manga's story. This version includes newly recorded dialog by the original voice actors, 
+            new sound effects, new OP/ED sequences, and a brand new HD video transfer.
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/anime/cover/medium/6033-POX2sGuDcySH.png',
+            vols: '100',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+
+        },
+    },
+    {
+        id: 6,
+        name: '',
+        preview_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx105778-82gwrvQV6OBc.png',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'Chainsaw Man',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/banner/105778-wk5qQ7zAaTGl.jpg',
+            content: `The name says it all! Denji's life of poverty is changed forever when he merges with his pet chainsaw dog, Pochita! Now he's living in the big city and an official Devil Hunter. 
+            But he's got a lot to learn about his new job and chainsaw powers!
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx105778-82gwrvQV6OBc.png',
+            vols: '11',
+            chapter: '97',
+            status: 'Finished',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+
+        },
+    },
+    {
+        id: 7,
+        name: '',
+        preview_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx30564-ucMJLXpxHJKI.jpg',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'Gantz',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/banner/30564-kRtGCFJctwXJ.jpg',
+            content: `A remastered version of Dragon Ball Z that adheres more to the manga's story. This version includes newly recorded dialog by the original voice actors, 
+            new sound effects, new OP/ED sequences, and a brand new HD video transfer.
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx30564-ucMJLXpxHJKI.jpg',
+            vols: '11',
+            chapter: '97',
+            status: 'Finished',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+        },
+    },
+    {
+        id: 8,
+        name: '',
+        preview_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx30003-gsp7GW4UYI2Q.jpg',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: '20th Century Boys',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/banner/30003-MBm3u1K1uHsi.jpg',
+            content: `Humanity, having faced extinction at the end of the 20th century, would not have entered the new millennium if it weren't for them. 
+            In 1969, during their youth, they created a symbol. In 1997, as the coming disaster slowly starts to unfold, that symbol returns. This is the story of a group of boys who try to save the world.
+
+            For Kenji, a simple convenience store manager who once dreamed of becoming a rock 'n' roll musician, a host of memories from his past come rushing back when one of his childhood friends mysteriously commits suicide. Could this new death be related to the rise of a bizarre new cult that's been implicated in several other murders and disappearances? Determined to dig deeper,
+             Kenji reunites with some of his old buddies in the hope of learning the truth behind it all..
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx30003-gsp7GW4UYI2Q.jpg',
+            vols: '11',
+            chapter: '97',
+            status: 'Finished',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+
+        },
+    },
+    {
+        id: 9,
+        name: '',
+        preview_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx54705-YDpCv7Y0KIcx.png',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'The Flowers of Evil',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/banner/54705-B4Lz3ktrnjGm.jpg',
+            content: `The story opens as middle school student Takao Kasuga receives an F on a math test. But he doesn't even seem 
+            to notice because he's too engrossed in surreptitiously reading Beaudelaire's The Flowers of Evil. And the day goes downhill from there. In a moment of weakness, he finds and takes home the gym clothes belonging to sweet, pretty Nanako Saeki on whom he has a major crush. Unfortunately for Takao, there's a witness to the theft: 
+            Nakamura, who has a huge chip on her shoulder and a sadistic streak.
+            As the saga unfolds, we see Takao struggling to decide whether to confess or cover up his misdeeds at the same time that he tries to win over the girl of his dreams, 
+            and avoid the blackmail attempts of Nakamura, his new ”BFF.”
+            Smart, funny, and emotionally engaging, The Flowers of Evil introduces a character who's not a hero, but just an ordinary teenager in search of true love and real friendship.
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx54705-YDpCv7Y0KIcx.png',
+            vols: '11',
+            chapter: '97',
+            status: 'Finished',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+
+        },
+    },
+    {
+        id: 10,
+        name: '',
+        preview_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx63327-zvK2l9DjCqK4.jpg',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'Tokyo Ghoul',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/banner/63327-vcEScp8Lisgi.jpg',
+            content: `Shy Ken Kaneki is thrilled to go on a date with the beautiful Rize. But it turns out that she’s only interested in his body—eating it, 
+            that is. When a morally questionable rescue transforms him into the first half-human half-Ghoul hybrid, 
+            Ken is drawn into the dark and violent world of Ghouls, which exists alongside our own.
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx63327-zvK2l9DjCqK4.jpg',
+            vols: '11',
+            chapter: '97',
+            status: 'Finished',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+
+        },
+    },
+    {
+        id: 11,
+        name: '',
+        preview_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx85486-INqnYx8gL3eX.jpg',
+        author: 'QHuy',
+        created: '2021/09/24',
+        title: 'My Hero Academia',
+        scores: '10.0',
+        link_direct: "./content/content0.html",
+        detail: {
+            background_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/banner/85486-4mTRih1qOdgk.jpg',
+            content: `Middle school student Izuku Midoriya wants to be a hero more than anything, but he hasn’t got an ounce of power in him. 
+            With no chance of ever getting into the prestigious U.A. High School for budding heroes, his life is looking more and more like a dead end. 
+            Then an encounter with All Might, the greatest hero of them all, gives him a chance to change his destiny…
+            `,
+            profile_pic: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/large/bx85486-INqnYx8gL3eX.jpg',
+            vols: '11',
+            chapter: '97',
+            status: 'Finished',
+            start_date: 'Jul 2, 2012',
+            end_data: 'Apr 25, 2016',
+            mangaka: 'Yuusei Matsui'
+
+
+        },
+    },
+];
