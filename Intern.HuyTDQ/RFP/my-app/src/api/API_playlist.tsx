44258e9c50ae7750c2c296aac34a6b04a7f0496b
Qhuy <quanghuy4919@gmail.com>
2021-10-15 09:13:59 +0700

    feat: created axios template

---
+++ Intern.HuyTDQ/RFP/my-app/src/api/API_playlist.tsx
@@ -0,0 +1,12 @@
+import axiosClient from './axiosClient'
+import queryString from 'query-string';
+
+const API_Playlist = {
+    getPlaylist : (data) =>{
+        const paramString = queryString.stringify(data);
+        const url = `https://www.googleapis.com/youtube/v3/playlists?${paramString}`;
+        return axiosClient(url);
+    }
+}
+
+export default API_Playlist;
