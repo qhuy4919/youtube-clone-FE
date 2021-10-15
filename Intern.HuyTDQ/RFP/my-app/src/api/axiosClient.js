f1bba3fde99ee5d94de561ec6d6115caf2eb0045
Qhuy <quanghuy4919@gmail.com>
2021-10-15 09:13:59 +0700

    feat: create axios template

---
+++ Intern.HuyTDQ/RFP/my-app/src/api/axiosClient.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+const axiosClient = axios.create({
+    baseURL: process.env.YOUTUBE_API_KEY,
+    headers: {
+        'content-type': 'application/json',
+        // 'Access-Control-Allow-Origin': '*',
+    },
+    
+    paramsSerializer: params => queryString.stringify(params),
+    });
+
+    axiosClient.interceptors.request.use(async (config) => {
+        // Handle token here ...
+        return config;
+    })
+    axiosClient.interceptors.response.use((response) => {
+        if (response && response.data) {
+            return response.data;
+        }
+        return response;
+    }, (error) => {
+    
+        if (error.response.status === 401){
+            throw error;
+        } 
+    }
+);
+
+export default axiosClient;
